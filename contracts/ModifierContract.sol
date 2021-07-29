fe39372c4ac2f8d7608e4f4a525d7a4d7ca5efe9
Nathan Rhoda <nathan.rhoda@hotmail.com>
2021-07-29 19:11:32 +0200

    syntax around events

--- contracts/ModifierContract.sol
+++ contracts/ModifierContract.sol
@@ -3,14 +3,14 @@ pragma solidity >=0.4.22 <0.9.0;
 contract ModifierContract {
     uint  public minimum_bidding_amount = 10;
 
-    modifier MustBeGreaterThanMinimum(uint amount) public view  {
-        if(amount <= minimum_bidding_amount ) {
+    modifier MustBeGreaterThanMinimum  {
+        if(msg.value <= minimum_bidding_amount ) {
             revert();
         }
         _;
     }
 
-    function bid() payable public MustBeGreaterThanMinimum return (bool) {
+    function bid() payable public MustBeGreaterThanMinimum returns (bool) {
         return true;            
     }
 
